20ba8412ad8bb8f827c7f64e2132de6c93727468
Tiago Oliveira <suubmundo@gmail.com>
2019-10-02 20:00:46 -0300

    session, spot, dashboard. booking ok

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+
+const uploadCofig = require('./config/upload');
 const SessionControler = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
+const upload = multer(uploadCofig);
 
 routes.post('/sessions', SessionControler.store);
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+routes.get('/dashboard', DashboardController.show);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
