77b21166873359f723a666868d6697ed3cea03a5
Tiago Oliveira <suubmundo@gmail.com>
2019-10-07 21:21:35 -0300

    frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 const app = express();
@@ -16,6 +17,9 @@ mongoose.connect('mongodb+srv://suubmundo:NgUy7WzdgqmAuAJH@aircnc-tesgy.mongodb.
 // req.params = Acessar route params (para edição e delete)
 // req.body = Acessar corpo da requisição (para criar e edição de registros)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
+
 app.listen(8000);
